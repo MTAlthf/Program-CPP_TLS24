165b7fcf14c3df39cbacbe29f6c4957b2fd1f098
MTAlthf <althaf109873@gmail.com>
2024-09-12 13:10:19 +0700

    2

--- bmi.cpp
+++ bmi.cpp
@@ -9,31 +9,42 @@ int main() {
 
     
     // input
-    cout << "Masukkan berat badan (kg): ";
-    cin >> berat;
-    cout << "Masukkan tinggi badan (m): ";
-    cin >> tinggi;
+    while (true) {
+
+            cout << "Masukkan berat badan (kg): ";
+            cin >> berat;
+            cout << "Masukkan tinggi badan (m): ";
+            cin >> tinggi;
+
+            if (berat <= 0 || tinggi <= 0) {
+                cout << "Data tidak valid, silahkan coba lagi!\n\n"; //untuk mengulang input jika data tidak valid 
+            }
+            else {
+                break;// untuk keluar dari loop input jika data sudah valid
+            }
+
+    }
     // rumus
     bmi = berat/pow(tinggi, 2);
 
     // output
     if (bmi < 18.5) {
-        cout << "BMI Anda: " << bmi << "\n" << "Anda kekurangan berat badan";
+        cout << "BMI Anda: " << bmi << "\n" << "Anda kekurangan berat badan\n";
     }
     else if (18.5 <= bmi && bmi <= 24.9) {
-        cout << "BMI Anda: " << bmi << "\n" << "Berat badan Anda Ideal";
+        cout << "BMI Anda: " << bmi << "\n" << "Berat badan Anda Ideal\n";
     }
     else if (25 <= bmi && bmi <= 29.9) {
-        cout << "BMI Anda: " << bmi << "\n" << "Anda kelebihan berat badan";
+        cout << "BMI Anda: " << bmi << "\n" << "Anda kelebihan berat badan\n";
     }
     else if (30 <= bmi && bmi <= 34.9) {
-        cout << "BMI Anda: " << bmi << "\n" << "Anda mengalami obesitas tingkat 1";
+        cout << "BMI Anda: " << bmi << "\n" << "Anda mengalami obesitas tingkat 1\n";
     }
     else if (35 <= bmi && bmi <= 39.9) {
-        cout << "BMI Anda: " << bmi << "\n" << " Anda mengalami obesitas tingkat 2";
+        cout << "BMI Anda: " << bmi << "\n" << " Anda mengalami obesitas tingkat 2\n";
     }
     else if (bmi >= 40) {
-        cout << "BMI Anda: " << bmi << "\n" << "Anda mengalami obesitas tingkat 3";
+        cout << "BMI Anda: " << bmi << "\n" << "Anda mengalami obesitas tingkat 3\n";
     }
 return 0;
     
---
+++ bmi2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    // Variabel
+    float berat, tinggi, bmi;
+
+    // input
+        cout << "Masukkan berat badan (kg): ";
+        cin >> berat;
+        cout << "Masukkan tinggi badan (m): ";
+        cin >> tinggi;
+    bmi = berat/pow(tinggi, 2);
+    
+    
+    
+    // rumus
+
+    // output
+    if (bmi < 18.5) {
+        cout << "BMI Anda: " << bmi << "\n" << "Anda kekurangan berat badan";
+    }
+    else if (18.5 <= bmi && bmi <= 24.9) {
+        cout << "BMI Anda: " << bmi << "\n" << "Berat badan Anda Ideal";
+    }
+    else if (25 <= bmi && bmi <= 29.9) {
+        cout << "BMI Anda: " << bmi << "\n" << "Anda kelebihan berat badan";
+    }
+    else if (30 <= bmi && bmi <= 34.9) {
+        cout << "BMI Anda: " << bmi << "\n" << "Anda mengalami obesitas tingkat 1";
+    }
+    else if (35 <= bmi && bmi <= 39.9) {
+        cout << "BMI Anda: " << bmi << "\n" << " Anda mengalami obesitas tingkat 2";
+    }
+    else if (bmi >= 40) {
+        cout << "BMI Anda: " << bmi << "\n" << "Anda mengalami obesitas tingkat 3";
+    }
+    else {
+        cout<< "Input tidak valid";
+    }
+return 0;
+}
